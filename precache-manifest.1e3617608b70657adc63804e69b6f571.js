0ad9594031011e1215311f4358326b7871875179
Fareena Ehtesham <shimmiehtesham229@gmail.com>
2020-06-16 05:45:05 -0400

    Updates

---
+++ precache-manifest.1e3617608b70657adc63804e69b6f571.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b07fabe2ef0a4fb19552db4047c080",
+    "url": "/Movies-Recommendation/index.html"
+  },
+  {
+    "revision": "87b7f5d766fa300a0c26",
+    "url": "/Movies-Recommendation/static/css/main.67416cd4.chunk.css"
+  },
+  {
+    "revision": "c31b3a5214aa6a1a7673",
+    "url": "/Movies-Recommendation/static/js/2.64ce18bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movies-Recommendation/static/js/2.64ce18bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87b7f5d766fa300a0c26",
+    "url": "/Movies-Recommendation/static/js/main.c5b53a51.chunk.js"
+  },
+  {
+    "revision": "640e77bf9a673183e7bf",
+    "url": "/Movies-Recommendation/static/js/runtime-main.3be38ee5.js"
+  }
+]);
